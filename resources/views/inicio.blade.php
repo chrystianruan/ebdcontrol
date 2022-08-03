413a7ccd4bf7b27cb9675b874e6c34e06cbeba20
Chrystian Ruan <chrystianr37@gmail.com>
2022-08-03 20:13:47 -0300

    ajustes no inicio

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -12,9 +12,9 @@
 <body>
 
 <p>Olá, {{auth()->user()-> name}}</p>
-@if((auth()->user()->id_nivel != 1) && (auth()->user()->id_nivel != 2) && auth()->user()->status == false)<a href="/classe/">Acessar o sistema de Secretário/Classe ou professor</a>@endif
-@if(auth()->user()->id_nivel == 2 && auth()->user()->status == false)<a href="/admin/">Acessar a página administrativa</a>@endif
-@if(auth()->user()->id_nivel == 1 && auth()->user()->status == false)<a href="/master/">Acessar a página master</a>@endif
+@if((auth()->user()->id_nivel != 1) && (auth()->user()->id_nivel != 2) && auth()->user()->status == false)<a href="/classe/">Acessar a área de Secretário/Classe ou professor</a>@endif
+@if(auth()->user()->id_nivel == 2 && auth()->user()->status == false)<a href="/admin/">Acessar a área Admin</a>@endif
+@if(auth()->user()->id_nivel == 1 && auth()->user()->status == false)<a href="/master/">Acessar a área Master</a>@endif
 @if (session('danger'))
             <div class="alert">
                 <font>{{session('danger')}}</font>
