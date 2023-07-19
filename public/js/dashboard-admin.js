ebbba6c4f29dc8540b861319ed7e4885df1a9728
Chrystian Ruan <chrystianr37@gmail.com>
2023-07-18 23:16:04 -0300

    ajustando detalhe na hora de chamar o metodo no js que altera o link que leva a geracao do pdf

--- public/js/dashboard-admin.js
+++ public/js/dashboard-admin.js
@@ -3,7 +3,11 @@ let dataSelected = document.getElementById("date");
 let linkVisualizarPdf = document.getElementById("a-visualizar-pdf")
 
 
-classeSelected.addEventListener("change", function () {
-    linkVisualizarPdf.href = "/admin/visualizar/pdf-folha-frequencia/"+classeSelected.value+"/"+dataSelected.value;
-});
 
+
+function changeLinkToGeneratePDF() {
+    linkVisualizarPdf.href = "/admin/visualizar/pdf-folha-frequencia/"+classeSelected.value+"/"+dataSelected.value;
+}
+
+classeSelected.addEventListener("change", changeLinkToGeneratePDF);
+dataSelected.addEventListener("change", changeLinkToGeneratePDF);
