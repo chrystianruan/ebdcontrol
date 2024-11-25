285b337f473172e5b89011f6491aa84f82a555f8
Hartur Souza <hartursouza3@gmail.com>
2024-11-24 21:34:42 -0300

    refactor: alterado pagina

--- app/Http/Controllers/ComumController.php
+++ app/Http/Controllers/ComumController.php
@@ -15,7 +15,11 @@ class ComumController extends Controller
 
     public function meusDados() : View {
 
-        $pessoa = Pessoa::findOrFail(auth()->user()->pessoa_id);
+        $pessoa = Pessoa::select('pessoas.*', 'ufs.nome as uf_nome', 'formations.nome as formation_nome')
+        ->join('ufs', 'pessoas.id_uf', '=', 'ufs.id')
+        ->join('formations', 'pessoas.id_formation', '=', 'formations.id')
+        ->where('pessoas.id', auth()->user()->pessoa_id)
+        ->firstOrFail();
 
         $view = 'meus-dados';
         return view('comum.meus-dados', compact('view', 'pessoa'));
--- resources/views/comum/meus-dados.blade.php
+++ resources/views/comum/meus-dados.blade.php
@@ -8,46 +8,67 @@
 
 <hr>
 
-<div class="table-responsive">
-  <table cellpadding="5" cellspacing="0">
-    <tr>
-      <th>Nome</th>
-      <td>{{$pessoa->nome}}</td>
-    </tr>
-    <tr>
-      <th>Data de Nascimento</th>
-      <td>{{date('d/m/Y', strtotime($pessoa->data_nasc))}}</td>
-    </tr>
-    <tr>
-      <th>Sexo</th>
-      <td>@if($pessoa->sexo == 1) Masculino @else Feminino @endif</td>
-    </tr>
-    <tr>
-      <th>Ocupação</th>
-      <td>{{$pessoa->ocupacao}}</td>
-    </tr>
-    <tr>
-      <th>Cidade</th>
-      <td>{{$pessoa->cidade}}</td>
-    </tr>
-    <tr>
-      <th>Telefone</th>
-      <td>{{$pessoa->telefone}}</td>
-    </tr>
-    @if($pessoa->paternidade_maternidade != null)
-    <tr>
-      <th>Paternidade/Maternidade</th>
-      <td>{{$pessoa->paternidade_maternidade}}</td>
-    </tr>
-    @endif
-    @if($pessoa->responsavel != null)
-    <tr>
-      <th>Responsável</th>
-      <td>{{$pessoa->responsavel}}</td>
-    </tr>
-    @endif
-  </table>
-</div>
-
+<form style="margin: 10px 0;" class="row g-3">
+  <div class="col-md-6">
+    <label for="nome" class="form-label">Nome</label>
+    <input type="text" class="form-control" id="nome" value="{{$pessoa->nome}}" disabled>
+  </div>
+  <div class="col-md-3">
+    <label for="data-de-nascimento" class="form-label">Data de Nascimento</label>
+    <input type="text" class="form-control" id="data-de-nascimento" value="{{date('d/m/Y', strtotime($pessoa->data_nasc))}}" disabled>
+  </div>
+  @if($pessoa->paternidade_maternidade != null)
+  <div class="col-12">
+    <label for="paternidade-maternidade" class="form-label">Paternidade/Maternidade</label>
+    <input type="text" class="form-control" id="paternidade-maternidade" value="{{$pessoa->paternidade_maternidade}}" disabled>
+  </div>
+  @endif
+  @if($pessoa->responsavel != null)
+  <div class="col-12">
+    <label for="responsavel" class="form-label">Responsavel</label>
+    <input type="text" class="form-control" id="responsavel" value="{{$pessoa->responsavel}}" disabled>
+  </div>
+  @endif
+  <div class="col-md-3">
+    <label for="phoneNumber" class="form-label">Telefone</label>
+    <input type="text" class="form-control" id="phoneNumber" value="{{$pessoa->telefone}}" disabled>
+  </div>
+  <div class="col-md-4">
+    <label for="ocupacao" class="form-label">Ocupação</label>
+    <input type="text" class="form-control" id="ocupacao" value="{{$pessoa->ocupacao}}" disabled>
+  </div>
+  <div class="col-md-4">
+    <label for="inputCity" class="form-label">Cidade</label>
+    <input type="text" class="form-control" id="inputCity" value="{{$pessoa->cidade}}" disabled>
+  </div>
+  <div class="col-md-4">
+    <label for="inputState" class="form-label">Estado</label>
+    <input type="text" class="form-control" id="inputState" value="{{$pessoa->uf_nome}}" disabled>
+  </div>
+  <div class="col-12">
+    <label for="formation" class="form-label">Formação</label>
+    <input type="text" class="form-control" id="formation" value="{{$pessoa->formation_name}}" disabled>
+  </div>
+  <div class="col-md-4">
+    <label for="gridCheck" class="form-label">Sexo</label>
+    <div class="d-flex align-items-center">
+      <div class="form-check me-3">
+        <input class="form-check-input" type="checkbox" id="gridCheckMasculino" disabled @if($pessoa->sexo == 1) checked @endif>
+        <label class="form-check-label" for="gridCheckMasculino">
+          Masculino
+        </label>
+      </div>
+      <div class="form-check">
+        <input class="form-check-input" type="checkbox" id="gridCheckFeminino" disabled @if($pessoa->sexo == 2) checked @endif>
+        <label class="form-check-label" for="gridCheckFeminino">
+          Feminino
+        </label>
+      </div>
+    </div>
+  </div>
+  <div class="col-12">
+    <button type="submit" class="btn btn-primary">Alterar</button>
+  </div>
+</form>
 
 @endsection
